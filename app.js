063195964a5b5a601c20a1c3656cf87fcfba5638
doctoramalvada <mydemoni@hotmail.com>
2019-06-07 11:14:25 -0500

    subiendo carpetas

---
+++ app.js
@@ -0,0 +1,50 @@
+
+
+
+     
+     
+    // array = ["The Plot in You","The Devil Wears Prada","Pierce the Veil","Norma Jean","The Bled","Say Anything","The Midway State","We Came as Romans","Counterparts","Oh, Sleeper","A Skylit Drive","Anywhere But Here","An Old Dog"];
+    
+    // function listE(){ 
+    //     let lista=document.getElementById("list"); 
+    //     array.forEach(function(data){
+    //     let linew= document.createElement("li");    
+    //     let contenido = document.createTextNode(data);
+    //     lista.appendChild(linew);
+    //     linew.appendChild(contenido);
+        
+
+    //     let arf = contenido
+     
+    //     })
+        
+    //     }
+    //    listE();
+    
+    
+  
+
+
+  
+
+
+
+//    let nearr = "{The Plot in You, The Devil Wears Prada, Pierce the Veil, Norma Jean, The Bled, Say Anything, The Midway State, We Came as Romans, Counterparts , Oh, Sleeper, A Skylit Drive, Anywhere But Here, An Old Dog }";
+//     let nothe = nearr.replace(/\bThe\b/g, "")
+//     let noA = nothe
+//      let noAs = noA.replace(/\bA\b/g, "");
+//      let noAns = noAs
+//        let noAnsr = noAs.replace(/\bAn\b/g, "");
+//     let list= noAnsr
+//     console.log(list);
+
+
+
+
+     
+
+
+  
+
+
+
